498a5abb54a94df3189ea581bee4e2407a93f907
eunchae <eunchae619@naver.com>
2021-07-18 18:01:29 +0900

    Video Edit 기능추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -15,14 +15,26 @@ export const watch = async (req, res) => {
     return res.render("watch", { pageTitle: video.title, video });
 }
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    return res.render("edit", { pageTitle: `Editing:` });
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found." })
+    }
+    return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 }
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found." })
+    }
+    video.title = title;
+    video.description = description;
+    video.hashtags = hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
+    await video.save();
     return res.redirect(`/videos/${id}`);
 }
 
@@ -36,7 +48,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            hashtags: hashtags.split(",").map((word) => `#${word}`),
+            hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`)),
             meta: {
                 views: 0,
                 rating: 0,
