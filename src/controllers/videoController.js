061191e41178fd863757687be5e1c04110a85c57
eunchae <eunchae619@naver.com>
2021-07-18 16:52:35 +0900

    watch Controller 수정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,10 +9,10 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
-    if (video) {
-        return res.render("watch", { pageTitle: video.title, video });
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found." })
     }
-    return res.render("404", { pageTitle: "Video not found." })
+    return res.render("watch", { pageTitle: video.title, video });
 }
 
 export const getEdit = (req, res) => {
