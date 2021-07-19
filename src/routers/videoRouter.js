336bcd88c26690d65b25ff622562acd97a7af068
eunchae <eunchae619@naver.com>
2021-07-19 16:46:14 +0900

    Video Delete 기능 추가

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
+import { watch, getEdit, postEdit, deleteVideo, getUpload, postUpload } from "../controllers/videoController";
 
 
 const videoRouter = express.Router();
@@ -7,6 +7,7 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 
