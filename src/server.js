0b8e93782bfc3c2c916d22455431a1fa6424944a
eunchae <eunchae619@naver.com>
2021-07-21 15:34:11 +0900

    SessionStore를 connect-mongo로 변경

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import { localMiddleware } from "./middlewares";
 import globalRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
@@ -27,6 +28,7 @@ app.use(session({
     secret: "Hello!",
     resave: true,
     saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
 }))
 app.use((req, res, next) => {
     req.sessionStore.all((error, sessions) => {
