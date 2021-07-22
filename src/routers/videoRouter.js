c9d1a06fb5cf085f29fd046eb9e31e0791bee1e4
eunchae <eunchae619@naver.com>
2021-07-22 22:59:56 +0900

    로그인/비로그인 접근 방지 Middleware 기능 추가

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,4 +1,8 @@
 import express from "express";
+import {
+    blockLoggedInUserMiddleware,
+    blockAnonymousUserMiddleware,
+} from "../middlewares";
 import { watch, getEdit, postEdit, deleteVideo, getUpload, postUpload } from "../controllers/videoController";
 
 
@@ -6,9 +10,9 @@ const videoRouter = express.Router();
 
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.route("/:id([0-9a-f]{24})/edit").all(blockAnonymousUserMiddleware).get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})/delete", blockAnonymousUserMiddleware, deleteVideo);
+videoRouter.route("/upload").all(blockAnonymousUserMiddleware).get(getUpload).post(postUpload);
 
 
 export default videoRouter;
