e6e3dea0dc577625a53ce6c9a5c5ec3b87804b25
eunche <eunchae619@naver.com>
2021-09-04 16:15:14 +0900

    static 설정 추가

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import globalRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-
 /*
 express 서버 생성
 */
@@ -15,40 +14,37 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", `${process.cwd()}/src/views`);
 
-
-
-
 /*
 Middleware 모음
 */
 const logger = morgan("dev");
-app.use(logger)
-app.use(express.urlencoded({ extended: true }))
-app.use(session({
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 10800000,   // 3시간
+      maxAge: 10800000, // 3시간
     },
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
-}))
+  })
+);
 app.use((req, res, next) => {
-    req.sessionStore.all((error, sessions) => {
-        next();
-    });
+  req.sessionStore.all((error, sessions) => {
+    next();
+  });
 });
 app.use(localMiddleware);
-
-
 
 /*
 사용 Router 모음
 */
-app.use("/uploads", express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
-
 
 export default app;
