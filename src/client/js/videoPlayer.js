d4620c91f57cbd59e2426768283c732604973003
eunche <eunchae619@naver.com>
2021-09-06 13:19:29 +0900

    커스텀 비디오 플레이어 -> 음소거 & 볼륨 조절 기능 추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,35 @@
+// DOM References
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
+const volumeRange = document.getElementById("volume");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+
+// 전역 변수
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handleTime = (event) => {};
+
+const handleVolumeChange = (event) => {
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  const { value } = event.target;
+  video.volume = value;
+  volumeValue = value;
+};
+
+const handleMuteClick = (event) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
 
 const handlePlayClick = (event) => {
   // if 동영상이 멈춰있으면 -> 재생 하고
@@ -15,11 +42,7 @@ const handlePlayClick = (event) => {
   playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handleMute = (event) => {};
-const handleTime = (event) => {};
-const handleVolume = (event) => {};
-
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handleMute);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
 time.addEventListener("click", handleTime);
-volume.addEventListener("click", handleVolume);
