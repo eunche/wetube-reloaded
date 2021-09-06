49de1fed5afd064119c7a4acbea53a684a09a07a
eunche <eunchae619@naver.com>
2021-09-06 12:20:15 +0900

    JS 파일 구조 변경

--- src/client/js/getFirstAlphabet.js
+++ src/client/js/getFirstAlphabet.js
@@ -1,5 +1,9 @@
+// 유저 프로필 avatar-default의 user.name을 맨앞글자로 변경시키는 코드
 const getFirstAlphabet = (text) => {
   return text.charAt(0);
 };
 
-export default getFirstAlphabet;
+try {
+  const avatarDefault = document.querySelector(".js-avatar-default");
+  avatarDefault.innerText = getFirstAlphabet(avatarDefault.innerText);
+} catch {}
--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,11 +1,2 @@
 // SCSS
 import "../scss/styles.scss";
-
-// JS
-import getFirstAlphabet from "./getFirstAlphabet";
-
-// 유저 프로필 avatar-default 맨앞글자로 변경시키는 코드
-try {
-  const avatarDefault = document.querySelector(".js-avatar-default");
-  avatarDefault.innerText = getFirstAlphabet(avatarDefault.innerText);
-} catch {}
